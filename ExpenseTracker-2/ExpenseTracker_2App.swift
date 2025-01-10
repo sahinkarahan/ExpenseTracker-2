5c7539b94aaa59f0b6d3ed0b0f04192c3fae5fd2
sahinkarahan <sahin.karahan@hotmail.com>
2025-01-10 16:44:28 +0300

    Initial Commit

---
+++ ExpenseTracker-2/ExpenseTracker_2App.swift
@@ -0,0 +1,20 @@
+//
+//  ExpenseTracker_2App.swift
+//  ExpenseTracker-2
+//
+//  Created by Şahin Karahan on 10.01.2025.
+//
+
+import SwiftUI
+
+@main
+struct ExpenseTracker_2App: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
